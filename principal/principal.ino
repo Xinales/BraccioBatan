4b376fe5959ab1daa17b4aec3b9795c0857e9e15
Xinales <sergiosc29@educastur.es>
2024-04-23 13:44:22 +0200

    Resumen funciones
    MIni resumen de las funciones que va a haber

--- principal/principal.ino
+++ principal/principal.ino
@@ -1,10 +1,13 @@
-//Sergio estuvo aquí
-//Pelayo estuvo aquí. (●'◡'●)
+// Estas son más o menos las funciones que va a tener el proceso al final:
 
 void setup() {
+
+// mover_pos_ini
 
 }
 
 void loop() {
+
+// girar_grado - detectar_bola (no hay -> volver a girar_grado) - coger_bola - lanzar_bola - mover_pos_ini
 
 }
