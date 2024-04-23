0be16be87ad4f7da4f5a80e92d5b346da16b10ba
Xinales <sergiosc29@educastur.es>
2024-04-23 13:38:31 +0200

    Revert "Resumen - Sergio"
    This reverts commit e868171e69c905046a1134dba7bbfcf46359db17.

--- principal/principal.ino
+++ principal/principal.ino
@@ -1,13 +1,9 @@
-// Estas son más o menos las funciones que va a tener el proceso al final:
+//Sergio estuvo aquí
 
 void setup() {
-
-// mover_pos_ini
 
 }
 
 void loop() {
-
-// girar_grado - detectar_bola (no hay -> volver a girar_grado) - coger_bola - lanzar_bola - mover_pos_ini
 
 }
